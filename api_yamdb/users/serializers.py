decb008260f1647bdb29078cdb12c8c35bf54e16
Игорь Литвиненко <marcato1407@yandex.ru>
2022-07-26 15:46:27 +0300

    Добавил модель user и сериализатор

---
+++ api_yamdb/users/serializers.py
@@ -0,0 +1,15 @@
+﻿from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'bio', 'role',]
+        extra_kwargs = {
+            'first_name': {'required': False},
+            'last_name': {'required': False},
+            'bio': {'required': False},
+        }
