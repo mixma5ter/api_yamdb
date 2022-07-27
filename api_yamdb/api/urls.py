a653f318ad5977f1e4cead2c9c4a761c874b8faf
Евгений Спиридонов <deejay999@yandex.ru>
2022-07-27 09:32:08 +0300

    Перенес код из users.urls в api.urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,24 +1,27 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from users.views import send_confirmation_code, send_token, UsersViewSet
 from .views import CommentViewSet, ReviewViewSet
 
-router = DefaultRouter()
-# Роутер users
+router_v1 = DefaultRouter()
+router_v1.register(r"users", UsersViewSet, basename="users")
 # Роутер genres
 # Роутер categories
 # Роутер titles
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router_v1.urls)),
+    path("v1/auth/signup/", send_confirmation_code),
+    path("v1/auth/token/", send_token),
 ]
