b4b7f74cae008181587a060c0ac0ecfcb4f3c413
OlgaNef <villlian@mail.ru>
2022-07-27 23:45:25 +0300

    часть кода nefedova

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,15 +3,18 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (CommentViewSet,
                     ReviewViewSet,
-                    send_confirmation_code,
+                   send_confirmation_code,
                     send_token,
+                    CategoryViewSet,
+                    GenreViewSet,
+                    TitleViewSet,
                     UserViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register(r"users", UserViewSet, basename="users")
-# Роутер genres
-# Роутер categories
-# Роутер titles
+router_v1.register(r'categories', CategoryViewSet)
+router_v1.register(r'genres', GenreViewSet)
+router_v1.register(r'titles', TitleViewSet)
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,24 +1,91 @@
 from django.db import models
 
 from users.models import User
+from .validators import validate_year
 
 
 class Genre(models.Model):
     """Класс, описывающий жанр."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True
+    )
 
-    pass
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ['name']
 
 
 class Category(models.Model):
     """Класс, описывающий категорию."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True
+    )
 
-    pass
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
 
 
 class Title(models.Model):
     """Класс, описывающий произведение."""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    year = models.IntegerField(
+        verbose_name='Дата выхода',
+        validators=[validate_year]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        null=True,
+        default=None
+    )
 
-    pass
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+        ordering = ['name']
 
 
 class Review(models.Model):
