5c3a6c3d1b22e7ab12bd241f9a4c14682ff70623
Евгений Спиридонов <deejay999@yandex.ru>
2022-07-27 08:29:59 +0300

    Создал сериализаторы, представления и эндпоинты моделей Review и Comment. Оставил комментарии для коллег.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from . import views
-
+from .views import CommentViewSet, ReviewViewSet
 
 router = DefaultRouter()
+# Роутер users
+# Роутер genres
+# Роутер categories
+# Роутер titles
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls))
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+# Представление для модели User
+
+
+# Представление для модели Genre
+
+
+# Представление для модели Category
+
+
+# Представление для модели Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Представление для модели Review.
+
+    Набор представлений, который обеспечивает действия по умолчанию
+    «создать», «получить», «обновить», «обновить частично», «удалить».
+    """
+
+    serializer_class = ReviewSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Представление для модели Comment.
+
+    Набор представлений, который обеспечивает действия по умолчанию
+    «создать», «получить», «обновить», «обновить частично», «удалить».
+    """
+
+    serializer_class = CommentSerializer
