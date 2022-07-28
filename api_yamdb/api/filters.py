88c5f134155641430f95ef683dd870892edd9b15
Игорь Литвиненко <marcato1407@yandex.ru>
2022-07-28 23:34:12 +0300

    немного подправил модели и сериализаторы. Добавил один пермишн. Изменил Вьюсеты

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,15 +6,15 @@ from reviews.models import Title
 class TitlesFilter(filters.FilterSet):
     name = filters.CharFilter(
         field_name='name',
-        lookup_expr='icontains'
+        lookup_expr='contains'
     )
     category = filters.CharFilter(
         field_name='category__slug',
-        lookup_expr='icontains'
+        lookup_expr='contains'
     )
     genre = filters.CharFilter(
         field_name='genre__slug',
-        lookup_expr='icontains'
+        lookup_expr='contains'
     )
 
     class Meta:
